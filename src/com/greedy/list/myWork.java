59db1ec206bf1b783ad507be0f4c093c52807891
wontak seok <wontaek22@gmail.com>
2021-11-22 18:43:32 +0900

    21.11.22
    추가사항

--- src/com/greedy/list/myWork.java
+++ src/com/greedy/list/myWork.java
@@ -1,13 +1,33 @@
 package com.greedy.list;
 
+import java.util.Random;
+
 public class myWork {
 
 	public void work() {
 		
+		Random ran = new Random();
+		
+		int num;
+		String[] myDo = {"옷장정리", "책상정리", "재봉틀판매", "책장정리", "만든 옷 정리"};
 		
 		
-		System.out.println("랜덤으로 나온 나의 할일 결과");
-		System.out.println();
+		
+//		for(int i = 0; i < 10; i++) {
+//			
+//			System.out.println("랜덤으로 나온 나의 할일 결과");
+//			System.out.println(myDo[i]);
+//		}
+//			System.out.println("내가 우선적으로 할 일은 " + myDo[0] + "입니다.");
+//////////////////////////////////////////////////////////////////////////////
+		
+		
+		for(String value : myDo) {
+			System.out.println();
+		}
+
+		
+		
 	}
 	
 }
